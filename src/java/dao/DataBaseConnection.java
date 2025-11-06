4faa724b4ab7589f66e2fe900ceb3204d3a361d3
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-06 11:35:33 -0500

    feat(database): implementar clase DataBaseConnection con patrón Singleton para manejo de conexión

--- src/java/dao/DataBaseConnection.java
+++ src/java/dao/DataBaseConnection.java
@@ -12,18 +12,6 @@ import java.sql.SQLException;
  *
  * @author Usuario
  */
-public class DataBaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/UsuarioService";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "12345678";
-    
-    public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("org.postgresql.Driver"); // carga el driver
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Error cargando el driver de postgress", e);
-        }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-    
+public interface DataBaseConnection {
+    Connection getConection();
 }
