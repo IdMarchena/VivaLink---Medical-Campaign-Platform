30c5fd9ac89da0911193d974e6116032942d96a7
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-06 11:35:42 -0500

    feat(factory): crear DaoFactory para retornar instancias de DAOs según tipo de base de datos

--- src/java/dao/DaoFactory.java
+++ src/java/dao/DaoFactory.java
@@ -16,29 +16,31 @@ import dao.RolDaoPostgre;
  */
 public class DaoFactory {
     public static UsurioDao getUsuario(String tipo) throws SQLException{
+        DataBaseConnection conn = DataBaseConnectionFactory.connection(tipo);
         switch(tipo.toLowerCase()){
             case "postgres" -> {
-                return new UsuarioDaoPostgre();
+                return new UsuarioDaoPostgre(conn);
             }
             case "mysql" -> {
-                return new UsuarioDaoMySql();
+                return new UsuarioDaoMySql(conn);
             }
             case "mongo" ->{
-                return new UsuarioDaoMongo();
+                return new UsuarioDaoMongo(conn);
             }
             default -> throw new IllegalArgumentException("tipo de dao no soportado: "+ tipo);
         }
     }
     public static RolDao getRol(String tipo) throws SQLException{
+        DataBaseConnection conn = DataBaseConnectionFactory.connection(tipo);
         switch(tipo.toLowerCase()){
             case "postgres" -> {
-                return new RolDaoPostgre();
+                return new RolDaoPostgre(conn);
             }
             case "mysql" -> {
-                return new RolDaoMysql();
+                return new RolDaoMysql(conn);
             }
             case "mongo" -> {
-                return new RolDaoMongo();
+                return new RolDaoMongo(conn);
             }
             default -> throw new IllegalArgumentException("tipo de dao no soportado: "+tipo);
         }
